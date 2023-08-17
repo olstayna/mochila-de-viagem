dcb7aadb2f1b2a02f0b33106b6b18e3ac3948d7b
Tayná Oliveira <tayna.oliveira.santana@gmail.com>
2023-08-16 22:23:27 -0300

    update no projeto

--- js/main.js
+++ js/main.js
@@ -12,14 +12,26 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    const existe = itens.find(elemento => elemento.nome === nome.value)
+
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-    criaElemento(itemAtual)
+    if (existe) {
+        itemAtual.id = existe.id
 
-    itens.push(itemAtual)
+        atualizaElemento(itemAtual)
+
+        itens[existe.id] = itemAtual
+    } else {
+        itemAtual.id = itens.length
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
 
     localStorage.setItem("itens", JSON.stringify(itens))
 
@@ -28,14 +40,19 @@ form.addEventListener("submit", (evento) => {
 })
 
 function criaElemento(item) {
-    const novoItem = document.createElement('li')
+    const novoItem = document.createElement("li")
     novoItem.classList.add("item")
 
-    const numeroItem = document.createElement('strong')
+    const numeroItem = document.createElement("strong")
     numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
 
     novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
